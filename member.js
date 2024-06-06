f8a6bf39cf98576a09619bef5260534a4c39744b
Jesus <jesusfrancisco@hotmail.es>
2024-06-06 20:21:22 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    var member = new Object();
+    member.name = "John Doe";
+    member.age = 20;
+    member.skills = ["HTML", "CSS", "JS", "PHP"];
+    return member;
+}
